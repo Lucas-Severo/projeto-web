546aeff66127430d5a96383ab61be34bb6f18859
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2020-11-21 19:19:35 -0400

    Alterado sgbd para postgresql (#3)

--- projeto-api/config/database.js
+++ projeto-api/config/database.js
@@ -4,8 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.db/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'teste_postgres'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'postgresql'),
+        ssl: env.bool('DATABASE_SSL', false)
       },
       options: {
         useNullAsDefault: true,
